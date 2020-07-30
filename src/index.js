00896b7eea364102a603129d49e7b1720abbed18
pipko-denis <pipko_work@mail.ru>
2020-07-30 15:14:27 +0300

    Main page draft

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-console.log(React);
+const el = (
+  <div>
+    <h1>My Todo list</h1>    
+    <input placeholder="search" />
+    <ul>
+      <li>Learn react</li>
+      <li>Build awsome app</li>
+    </ul>
+  </div>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(el,document.getElementById("root"));
